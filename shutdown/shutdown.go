c0f20a8af2b0b3f51aaf701f023095a0d17c8627
colussim <mcolussi@bluewin.ch>
2025-01-10 13:19:37 +0100

    Update shutdown

--- shutdown/shutdown.go
+++ shutdown/shutdown.go
@@ -14,13 +14,13 @@ import (
 )
 
 func main() {
-	// Configure a new Raspberry Pi instance
+	// Set up the Raspberry Pi adaptor
 	r := raspi.NewAdaptor()
 
-	// Configure GPIO Pin 3 (P1_5) as an input with a pull-up resistor
-	button := gpio.NewButtonDriver(r, "3")
+	// Create a GPIO driver for the button connected to GPIO 3
+	button := gpio.NewDriver(r, "3")
 
-	// Creating a Gobot robot
+	// Create the Gobot robot
 	robot := gobot.NewRobot("buttonBot",
 		[]gobot.Connection{r},
 		[]gobot.Device{button},
@@ -28,35 +28,30 @@ func main() {
 
 	// When the button is pressed, execute shutdown
 	button.On(gpio.ButtonPush, func(data interface{}) {
-		fmt.Println("Button pressed, initiating shutdown...")
+		fmt.Println("Button pressed. Initiating shutdown...")
 
-		// Perform shutdown, signaling shutdown listeners
+		// Set a timeout if you wish to delay
 		shutdown.SetTimeout(time.Second * 4)
 		shutdown.Shutdown()
 
-		// Wait for all shutdown handlers to complete
+		// Wait for shutdown handlers to complete
 		shutdown.Wait()
-
 		fmt.Println("Shutdown completed.")
-		os.Exit(0) // Exit the program after shutdown
+		os.Exit(0) // Clean exit of the application
 	})
 
-	// Manage interrupt signals for a clean stop
+	// Manage interrupt signals for clean exit
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
-		fmt.Println("Interrupt received, initiating shutdown...")
-
-		// Signal the shutdown
+		fmt.Println("Interrupt received! Shutting down...")
 		shutdown.Shutdown()
-
-		// Wait for all shutdown handlers to complete
 		shutdown.Wait()
-
-		os.Exit(0) // Exit the program after shutdown
+		os.Exit(0)
 	}()
 
-	// Start the robot
+	// Print startup message and start the robot
+	fmt.Println("Starting the robot... Press the button to shutdown.")
 	robot.Start()
 }
