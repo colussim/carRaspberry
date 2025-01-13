6a50e65aa13980adccfbf74a51399042287b386d
colussim <mcolussi@bluewin.ch>
2025-01-13 09:27:24 +0100

    Add Install script

---
+++ amplipower/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Path to service files (change this if necessary)
+SERVICE_DIR=/etc/systemd/system
+AMPLI_POWER_SERVICE=services/amplipower.service
+AMPLI_STOP_SERVICE=services/amplipower-stop.service
+
+echo "Installing services..."
+
+# Copy service files to /etc/systemd/system
+cp $AMPLI_POWER_SERVICE $SERVICE_DIR/
+cp $AMPLI_STOP_SERVICE $SERVICE_DIR/
+
+# Reload systemd configuration files 
+sudo systemctl daemon-reload
+
+# Enable services at startup  
+sudo systemctl enable amplipower.service
+sudo systemctl enable amplipower-stop.service
+
+
